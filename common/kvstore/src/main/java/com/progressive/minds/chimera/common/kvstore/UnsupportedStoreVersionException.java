c57eb5209cb2d6333caf4120a9f2d9318cd51b3f
abhinav.official <abhinav.official@gmail.com>
2024-12-15 16:26:51 +0000

    KV initial update

---
+++ common/kvstore/src/main/java/com/progressive/minds/chimera/common/kvstore/UnsupportedStoreVersionException.java
@@ -0,0 +1,9 @@
+package com.progressive.minds.chimera.common.kvstore;
+
+import com.progressive.minds.chimera.common.tags.annotation.Private;
+
+import java.io.IOException;
+
+@Private
+public class UnsupportedStoreVersionException extends IOException {
+}
