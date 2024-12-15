cad24ca786a390a14de59805dd1ce9381533ffcc
Kumar, Manish <manish.kumar.gupta@outlook.com>
2024-12-15 21:26:00 +0530

    Added Temporal Example as well as changes the java Version to 17 for Flink Services

---
+++ core/apacheComet/src/main/java/com/progressive/minds/chimera/core/apacheComet/TemporalSamples/MyWorkflowImpl.java
@@ -0,0 +1,28 @@
+/*
+package com.progressive.minds.chimera.core.apacheComet.TemporalSamples;
+
+
+
+import com.progressive.minds.chimera.core.dataSource.sourceTypes.FileReader;
+// import com.progressive.minds.chimera.core.temporal.example.workflows.MyWorkflow;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+import static com.progressive.minds.chimera.core.apacheComet.TemporalSamples.SharedSparkSession.spark;
+
+public class MyWorkflowImpl implements MyWorkflow {
+    @Override
+    public String myFunction(String input) {
+        System.out.print("Started Executing myFUnction with INput " + input);
+        Dataset<Row> dataFrame = spark.emptyDataFrame();
+        dataFrame = new FileReader().read("parquet", spark, "ParquetReaderTest",
+                input, "", "",
+                null, "", "",
+                "", 0);
+        System.out.print("Completed Executing myFUnction with INput " + input);
+        return String.valueOf(dataFrame.count());
+    }
+ }*/
