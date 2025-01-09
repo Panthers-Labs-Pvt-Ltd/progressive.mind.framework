73f2c4d07afcd346970c0f6e5211f03b9f87982f
Vivek T <vivekt1801@gmail.com>
2025-01-09 20:41:14 +0530

    FRAM-187: API related changes

---
+++ core/api-service/src/main/java/com/progressive/minds/chimera/controller/ExternalController.java
@@ -0,0 +1,20 @@
+package com.progressive.minds.chimera.controller;
+
+import com.progressive.minds.chimera.service.ExternalService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExternalController {
+
+    private final ExternalService externalService;
+
+    public ExternalController(ExternalService externalService) {
+        this.externalService = externalService;
+    }
+
+    @GetMapping("/fetch-data")
+    public String fetchData() {
+        return externalService.callExternalApi();
+    }
+}
---
+++ core/api-service/src/main/java/com/progressive/minds/chimera/entity/DbEntity.java
@@ -0,0 +1,8 @@
+package com.progressive.minds.chimera.entity;
+
+/**
+ * The type Db entity.
+ */
+@SuppressWarnings("squid:S1214")
+public interface DbEntity {
+}
