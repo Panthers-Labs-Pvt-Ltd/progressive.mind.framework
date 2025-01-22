c8f738c7445ad28b1815fa813eac2a426a2ec9ba
Vivek T <vivekt1801@gmail.com>
2025-01-22 20:16:19 +0530

    FRAM-187: Fixed basic auth and role based access!

--- core/api-service/src/main/java/com/progressive/minds/chimera/controller/PipelineController.java
+++ core/api-service/src/main/java/com/progressive/minds/chimera/controller/PipelineController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -102,6 +103,7 @@ public class PipelineController {
 
   // Delete request -delete pipeline
   @DeleteMapping("/delete/{pipeLineName}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<GenericResponse> deletePipeline(@PathVariable("pipeLineName") String pipelineName) {
     if (pipelineService.deletePipeline(pipelineName) == 0) {
       GenericResponse genericResponse = GenericResponse.builder()
--- core/api-service/src/main/java/com/progressive/minds/chimera/security/ChimeraSecurityConfig.java
+++ core/api-service/src/main/java/com/progressive/minds/chimera/security/ChimeraSecurityConfig.java
@@ -1,28 +1,21 @@
 package com.progressive.minds.chimera.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
+@Profile({"prod", "default"})
+public class ChimeraSecurityConfig {
 
-  @Value("${security.userName}")
-  private String userName;
-  @Value("${security.password}")
-  private String password;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -50,15 +43,5 @@ public class SecurityConfig {
     JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
     converter.setJwtGrantedAuthoritiesConverter(jwt -> new KeycloakRoleConverter().convertRoles(jwt));
     return converter;
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails admin = User.builder()
-        .username(userName)
-        .password(password)
-        .roles("ADMIN")
-        .build();
-    return new InMemoryUserDetailsManager(admin);
   }
 }
