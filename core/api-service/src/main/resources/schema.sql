1a35c98d24d753b724e72ae4e21249a2cc42c8ec
Vivek T <vivekt1801@gmail.com>
2025-02-03 22:17:53 +0530

    FRAM-187: Code change to remove swagger from auth

--- core/api-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE PIPELINES (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    created_date TIMESTAMP,
-    last_modified_date TIMESTAMP,
-    frequency VARCHAR(255),
-    schedule VARCHAR(255)
-);
