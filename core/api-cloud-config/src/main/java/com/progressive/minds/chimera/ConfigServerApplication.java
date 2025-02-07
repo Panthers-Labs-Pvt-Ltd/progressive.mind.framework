b5c8c165bf4d96b5585a2b626f9054f533cec7e8
Chimera <progressive.mind.chimera@gmail.com>
2025-02-07 01:34:36 +0000

    Compiled code and added deequ in the framework

--- core/api-cloud-config/src/main/java/com/progressive/minds/chimera/ConfigServerApplication.java
+++ core/api-cloud-config/src/main/java/com/progressive/minds/chimera/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.progressive.minds;
+package com.progressive.minds.chimera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
