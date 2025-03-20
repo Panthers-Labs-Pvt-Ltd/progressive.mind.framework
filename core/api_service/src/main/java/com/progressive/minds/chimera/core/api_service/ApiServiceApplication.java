4a68175b71d65828d01b117d6d159f25403a668f
Vivek <infrachimera@gmail.com>
2025-03-20 11:51:08 +0530

    FRAM-Starters: These starters will improve the developers experience

--- core/api_service/src/main/java/com/progressive/minds/chimera/core/api_service/ApiServiceApplication.java
+++ core/api_service/src/main/java/com/progressive/minds/chimera/core/api_service/ApiServiceApplication.java
@@ -1,15 +1,12 @@
 package com.progressive.minds.chimera.core.api_service;
 
-import com.progressive.minds.keyclock.KeycloakAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
     org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
-@ImportAutoConfiguration(KeycloakAutoConfiguration.class)
 public class ApiServiceApplication {
 
   public static void main(String[] args) {
